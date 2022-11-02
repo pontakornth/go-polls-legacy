4d277dd3a4cfca51d95629586202a6afefa6dad8
pontakornth <pontakorn61@gmail.com>
2022-11-02 10:50:45 +0700

    feat: Add configuration

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/spf13/viper"
 )
 
 func handle(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +15,28 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"message": "not found"})
 }
 func main() {
+	initTimezone()
+	initConfig()
 	http.HandleFunc("/", handle)
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func initTimezone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+	time.Local = ict
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.SetEnvPrefix("POLLS")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
